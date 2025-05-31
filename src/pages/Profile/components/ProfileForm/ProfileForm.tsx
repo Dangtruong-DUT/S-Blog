49193f11f51f262fc7391132af82f0837261b473
TapLamIT <ndtrg281@gmail.com>
2025-06-01 03:13:13 +0700

    refactor: Simplify ProfileForm component by removing unused props and imports

--- src/pages/Profile/components/ProfileForm/ProfileForm.tsx
+++ src/pages/Profile/components/ProfileForm/ProfileForm.tsx
@@ -3,20 +3,13 @@ import { useQuery } from '@tanstack/react-query'
 import classNames from 'classnames/bind'
 
 import styles from './ProfileForm.module.scss'
-import { User } from 'src/types/user.type'
 import { AppContext } from 'src/contexts/app.context'
 import userApi from 'src/apis/user.api'
-import ProfileInfoSection from './components/ProfileInfoSection'
 import SocialLinksManager from './components/SocialLinksManager'
 
 const cx = classNames.bind(styles)
 
-interface Props {
-    userData: User
-    onCancel: () => void
-}
-
-const ProfileForm = ({ userData, onCancel }: Props) => {
+const ProfileForm = () => {
     const { profile: currentData } = useContext(AppContext)
 
     // Queries
@@ -28,7 +21,6 @@ const ProfileForm = ({ userData, onCancel }: Props) => {
 
     return (
         <div className={cx('formContainer')}>
-            {/* <ProfileInfoSection userData={userData} profile={profile} onCancel={onCancel} /> */}
             {/* Social Links Section */}
             <SocialLinksManager profileId={profile?.id} />
         </div>
